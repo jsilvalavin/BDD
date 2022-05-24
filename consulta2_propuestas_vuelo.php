9e85050fcfa6ec0f70300fc0b232ba9a894b0d66
José Silva <89276993+jsilvalavin@users.noreply.github.com>
2022-05-23 22:13:36 -0400

    Add files via upload

--- consulta2_propuestas_vuelo.php
+++ consulta2_propuestas_vuelo.php
@@ -8,7 +8,9 @@
     $codigo_destino = $_POST["codigo_destino"];
 
     $query = "SELECT vuelos.id FROM vuelos, aerodromos_salida, aerodromos_llegada 
-    WHERE vuelos.propuesta_vuelo_id =  aerodromos_salida.propuesta_vuelo_id AND aerodromos_llegada.propuesta_vuelo_id = vuelos.propuesta_vuelo_id AND vuelos.estado = 'aceptado' AND aerodromos_llegada.id_aerodromo = $codigo_destino AND aerodromos_salida = $aerodromo; ";
+    WHERE vuelos.propuesta_vuelo_id =  aerodromos_salida.propuesta_vuelo_id AND 
+    aerodromos_llegada.propuesta_vuelo_id = vuelos.propuesta_vuelo_id AND vuelos.estado = 'aceptado'
+     AND aerodromos_llegada.id_aerodromo = $codigo_destino AND aerodromos_salida.id_aerodromo = $aerodromo; ";
     $result = $db -> prepare($query);
     $result -> execute();
     $propuestas = $result -> fetchAll();
